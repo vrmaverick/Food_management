64da08dc1d4011d1f3a956fb6b33dc098d840b15
vrmaverick <119831329+vrmaverick@users.noreply.github.com>
2023-03-14 22:57:40 +0530

    Add files via upload

---
+++ head.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Website</title>
+</head>
+<body>
+	<header>
+		<nav>
+			<ul>
+				<li><a href="https://awintoursandtravels.com/zerowastage/">Home</a></li>
+			</ul>
+		</nav>
+	</header>
+</body>
+</html>
